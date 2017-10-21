812b9ba7bf7ada3d22bb3c1c6af8ac02fcc3ebd0
Joshua Downey <jdugdown@users.noreply.github.com>
2017-10-20 23:10:13 -0400

    added copyright line

--- footer.php
+++ footer.php
@@ -10,6 +10,17 @@
 ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 text-center text-sm-left">
+					<?php // TODO: footer navigation ?>
+				</div>
+
+				<div class="col-lg-4 text-center text-sm-right">
+					<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url('/') ); ?>" rel="home"><?php bloginfo('name'); ?></a>.</p>
+				</div>
+			</div>
+		</div>
 	</footer> <!-- #colophon -->
 
 	<?php wp_footer(); ?>
