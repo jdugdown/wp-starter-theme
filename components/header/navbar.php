821ba8a21efba42233afebbf73f0772fe0a57174
Joshua Downey <downey.josh@gmail.com>
2017-11-03 15:10:30 -0400

    wrap items in a fluid container

--- components/header/navbar.php
+++ components/header/navbar.php
@@ -8,12 +8,14 @@
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-	<a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>" rel="home"><?php bloginfo('title'); ?></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
+	<div class="container-fluid">
+		<a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>" rel="home"><?php bloginfo('title'); ?></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<?php wpst_primary_navigation(); ?>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<?php wpst_primary_navigation(); ?>
+		</div>
 	</div>
 </nav>
