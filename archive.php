47e8d2e70052f15e26b910005a6a0d7632b3e404
Joshua Downey <downey.josh@gmail.com>
2017-11-21 17:00:54 -0500

    add page header

--- archive.php
+++ archive.php
@@ -16,28 +16,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header(); ?>
 
 	<main id="index" role="main">
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<?php
+				the_archive_title( '<h1 class="page-title display-4">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description lead">', '</div>' );
+				?>
+			</div>
+		</div>
+
 		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-xl-9">
 
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
+					<?php if ( have_posts() ) : ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'components/post/single', 'excerpt' ); ?>
 
-					<?php get_template_part( 'components/post/single', 'excerpt' ); ?>
+						<?php endwhile; ?>
 
-				<?php endwhile; ?>
+						<?php // TODO: add pagination func here ?>
 
-				<?php // TODO: add pagination func here ?>
+					<?php else : ?>
 
-			<?php else : ?>
+						<h2 class="single-title"><?php _e('No posts yet', 'wpst'); ?></h2>
 
-				<h1 class="single-title"><?php _e('No posts yet', 'wpst'); ?></h1>
+					<?php endif; ?>
 
-			<?php endif; ?>
+				</div>
 
+				<?php get_sidebar(); ?>
+
+			</div>
 		</div>
 	</main> <!-- main -->
 
