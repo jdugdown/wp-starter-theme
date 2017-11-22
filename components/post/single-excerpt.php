b7a0d5c5d4d06c02cd215f21220221e6bd018431
Joshua Downey <downey.josh@gmail.com>
2017-11-22 15:32:24 -0500

    change featured image size

--- components/post/single-excerpt.php
+++ components/post/single-excerpt.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('card'); ?>>
-	<?php the_post_thumbnail( 'large', array( 'class' => 'card-img-top' ) ); ?>
+	<?php the_post_thumbnail( 'wpst-post-featured', array( 'class' => 'card-img-top' ) ); ?>
 
 	<header class="entry-header card-body">
 		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
