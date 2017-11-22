cc55e6fa61a729eb478b38c1ec80314c1def0e90
Joshua Downey <downey.josh@gmail.com>
2017-11-22 15:56:34 -0500

    remove featured image

--- components/post/single.php
+++ components/post/single.php
@@ -17,8 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
-		<?php the_post_thumbnail( 'large', array( 'class' => 'aligncenter' ) ); ?>
-
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<?php get_template_part( 'components/post/meta' ); ?>
